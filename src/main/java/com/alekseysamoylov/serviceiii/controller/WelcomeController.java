587f39e3623e1115dc938e110fe169b7819ef9ba
AlekseySamoylov <alekseysamoylov89@gmail.com>
2016-11-06 11:59:20 +0500

    Delete unnecessary classes

--- src/main/java/com/alekseysamoylov/serviceiii/controller/WelcomeController.java
+++ src/main/java/com/alekseysamoylov/serviceiii/controller/WelcomeController.java
@@ -1,11 +1,9 @@
 package com.alekseysamoylov.serviceiii.controller;
 
 import com.alekseysamoylov.serviceiii.entity.CompanyPositionOnMap;
-import com.alekseysamoylov.serviceiii.entity.Price;
 import com.alekseysamoylov.serviceiii.entity.TestClass;
 import com.alekseysamoylov.serviceiii.repository.TestCustomRepository;
 import com.alekseysamoylov.serviceiii.service.CompanyPositionOnMapService;
-import com.alekseysamoylov.serviceiii.service.PriceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,16 +18,13 @@ import java.util.List;
 @Controller
 public class WelcomeController {
 
-    private PriceService priceService;
     private CompanyPositionOnMapService companyPositionOnMapService;
     private TestCustomRepository testCustomRepository;
 
     @Autowired
     public WelcomeController(
-            PriceService priceService,
             CompanyPositionOnMapService companyPositionOnMapService,
             TestCustomRepository testCustomRepository) {
-        this.priceService = priceService;
         this.companyPositionOnMapService = companyPositionOnMapService;
         this.testCustomRepository = testCustomRepository;
     }
@@ -37,13 +32,6 @@ public class WelcomeController {
     @RequestMapping(value = "/")
     public String goWelcome() {
         return "index";
-    }
-
-    @CrossOrigin
-    @RequestMapping(value = "/prices")
-    @ResponseBody
-    public List<Price> getPrices() {
-        return priceService.findAll();
     }
 
     @CrossOrigin
